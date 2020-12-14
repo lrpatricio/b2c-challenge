07964669d212cff9acdabf7343a4c78928103ca0
Marcos <marcosvto1@gmail.com>
2020-12-14 15:07:40 -0300

    task: ignore ssl error axios

--- spa/src/services/api.js
+++ spa/src/services/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
-
+import https from 'https';
 
 const api = axios.create({
-  baseURL: 'http://andremart.com.br'
+  baseURL: 'http://andremart.com.br',
+  httpsAgent: new https.Agent({  
+    rejectUnauthorized: false
+  })
 });
 
 api.defaults.headers.common['Content-Type'] = 'application/json';
