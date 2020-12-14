8a910dc95552118362501571632eaa825fca4b6e
andremartds <andremartinsdsilva@gmail.com>
2020-12-13 23:10:41 -0300

    task: testes com a api

--- api/src/routes.php
+++ api/src/routes.php
@@ -28,7 +28,7 @@ $app->add(function ($request, $handler) {
     return $response
         ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
 });
 
 #ADMIN
